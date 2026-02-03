b1299ba43929d010057d3592dd7c6af0144f9426
Khaled_Aldumaini <khaledaldumaini258@gamil.com>
2026-02-03 19:01:08 +0300

    just for testing

--- Level 1/Probelm 1.cpp
+++ Level 1/Probelm 1.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
 
+/*
+	Problem: Print your name
+*/
+
 void PrintName(string Name) {
 
 	cout << "\nYour name is " << Name << endl;
+	cout << "hello bro";
+	// last line for test
 
 }
 
