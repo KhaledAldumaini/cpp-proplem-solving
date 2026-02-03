b79c40dd5bb1714870f77f778d3f98eecfcc40d1
Khaled_Aldumaini <khaledaldumaini258@gamil.com>
2026-02-03 17:30:40 +0300

    Solve problem 1: writing my name

---
+++ Level 1/Probelm 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void PrintName(string Name) {
+
+	cout << "\nYour name is " << Name << endl;
+
+}
+
+int main() {
+
+	PrintName("Khaled");
+
+	return 0;
+}
