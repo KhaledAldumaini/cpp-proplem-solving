e720e23672d96e4bb794f695dc216a16dc76d62d
Khaled_Aldumaini <khaledaldumaini258@gamil.com>
2026-02-05 17:05:23 +0300

    Updating the solution of problem 5

---
+++ Level 1/Problem 6.cpp
@@ -0,0 +1,52 @@
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+/*
+	Problem: Write a program to get the user full name then print it by reverse way
+*/
+
+struct stFullName {
+	string FirstName;
+	string LastName;
+};
+
+stFullName ReadFullName() {
+
+	stFullName FullName;
+
+	cout << "\nPlease enter first name: ";
+	cin >> FullName.FirstName;
+	cout << "\nEnter your last name: ";
+	cin >> FullName.LastName;
+
+	return FullName;
+}
+
+string GetFullName(stFullName FullNameInfo, bool IsRevers = false) {
+
+	string FullName = "";
+
+	if(IsRevers)
+		FullName = FullNameInfo.LastName + " " + FullNameInfo.FirstName;
+	else
+		FullName = FullNameInfo.FirstName + " " + FullNameInfo.LastName;
+
+	return FullName;
+
+}
+
+void PrintFullName(string FullName) {
+
+	cout << "\nYour full name is: " << FullName << endl;
+
+}
+
+int main() {
+
+	PrintFullName(GetFullName(ReadFullName()));
+
+	return 0;
+}
